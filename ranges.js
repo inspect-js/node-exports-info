e98260dc78a7e969c4fa0d868934066865f344e2
Jordan Harband <ljharb@gmail.com>
2022-03-21 23:02:30 -0700

    [Fix] correct category version ranges

--- ranges.js
+++ ranges.js
@@ -4,10 +4,10 @@ module.exports = {
 	// TODO: add "JSON imports"
 	'>= 17': 'pattern-trailers-no-dir-slash',
 	'^14.19 || ^16.9': 'pattern-trailers',
-	'^12.20 || 15.x || 16.0 - 16.8': 'patterns',
-	'14.13 - 14.18': 'broken-dir-slash-patterns',
-	'^13.14 || 14.0 - 14.12': 'broken-dir-slash-conditions',
-	'12.17 - 12.19 || 13.7 - 13.13': 'conditions',
+	'^12.20 || 14.13 - 14.18 || 15.x': 'broken-dir-slash-patterns',
+	'16.0 - 16.8': 'patterns',
+	'12.17 - 12.19 || ^13.14 || 14.0 - 14.12': 'broken-dir-slash-conditions',
+	'13.7 - 13.13': 'conditions',
 	'13.3 - 13.6': 'experimental',
 	'13.0 - 13.2': 'broken',
 	'< 12.17': 'pre-exports'
