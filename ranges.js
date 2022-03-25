cc9f891f8679a1e4b1063d2d91546b72f82be011
Jordan Harband <ljharb@gmail.com>
2022-03-24 23:17:48 -0500

    [Fix] `node v13.13 also has broken dir-slash

--- ranges.js
+++ ranges.js
@@ -5,8 +5,8 @@ module.exports = {
 	'>= 17': 'pattern-trailers-no-dir-slash',
 	'^14.19 || ^16.9': 'pattern-trailers',
 	'^12.20 || 14.13 - 14.18 || 15.x || 16.0 - 16.8': 'patterns',
-	'12.17 - 12.19 || ^13.14 || 14.0 - 14.12': 'broken-dir-slash-conditions',
-	'13.7 - 13.13': 'conditions',
+	'12.17 - 12.19 || ^13.13 || 14.0 - 14.12': 'broken-dir-slash-conditions',
+	'13.7 - 13.12': 'conditions',
 	'13.3 - 13.6': 'experimental',
 	'13.0 - 13.2': 'broken',
 	'< 12.17': 'pre-exports'
